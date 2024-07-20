5ee4df2b76ccd1efe5c0a1f6d5c3c36b93a0d54f
BoulderPro17 <120671960+BoulderPro17@users.noreply.github.com>
2024-07-20 21:49:37 +0200

    Add files via upload

---
+++ kalash.js
@@ -0,0 +1,16 @@
+module.exports = {
+  token: process.env.token,
+  port: "3000",
+  prefix: "?",
+  client: "1188403176433524766",
+  client_id: "1188403176433524766",
+  client_secret: process.env.cs,
+  redirect_uri: "https://93b68f5e-9f55-4c22-8aa5-24fb7cebfb9c-00-18n1kuw6eejo.kirk.replit.dev/",
+  footer: "BoulderPro17",
+  support: "https://discord.gg/S4tJ3pM6Ns",
+  wehbook: process.env['wehbhook'], //wehbook de logs
+  owners: ["1060965300541063299"],
+  authLink: "https://discord.com/api/oauth2/authorize?client_id=1188403176433524766&response_type=code&redirect_uri=https%3A%2F%2F93b68f5e-9f55-4c22-8aa5-24fb7cebfb9c-00-18n1kuw6eejo.kirk.replit.dev%2F&scope=identify+guilds.join",
+  joinLink: "https://discord.com/api/oauth2/authorize?client_id=1188403176433524766&permissions=8&scope=bot",
+
+}
